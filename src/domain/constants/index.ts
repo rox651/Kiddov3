79aad90db03bede004b2c67a6b7111ee9edc4c1c
homero <homerojose69@gmail.com>
2025-06-23 19:32:23 -0500

    feat: update projects showcase section

--- src/domain/constants/index.ts
+++ src/domain/constants/index.ts
@@ -1,4 +1,4 @@
 export const BASE_BACKEND_URL =
   import.meta.env.BASE_BACKEND_URL || "http://localhost:1337";
 export const BACKEND_URL =
-  import.meta.env.GRAPHQL_API_URL || "http://localhost:1337/graphql";
+  import.meta.env.BACKEND_URL || "http://localhost:1337/graphql";
--- src/infrastructure/adapters/project/ProjectGraphQLAdapter.ts
+++ src/infrastructure/adapters/project/ProjectGraphQLAdapter.ts
@@ -1,53 +1,63 @@
 import type { IProjectRepository } from "../../../domain/repositories/project/IProjectRepository";
 import type {
-   Project,
-   ProjectShowCase,
-   QueryProjectsResponse,
-   QueryProjectsShowCaseResponse,
+  Project,
+  ProjectShowCase,
+  QueryProjectsResponse,
+  QueryProjectsShowCaseResponse,
 } from "../../../domain/entities/project";
 
 import client from "../../graphql/client";
-import { queryProjects } from "../../graphql/queries/queriesProjects";
+import {
+  queryProjects,
+  queryProjectsShowcase,
+} from "../../graphql/queries/queriesProjects";
 
 export function createProjectGraphQLRepository(): IProjectRepository {
-   const getAllProjects = async (): Promise<Project[]> => {
-      const result = await client.query<QueryProjectsResponse>(queryProjects, {}).toPromise();
+  const getAllProjects = async (): Promise<Project[]> => {
+    const result = await client
+      .query<QueryProjectsResponse>(queryProjects, {})
+      .toPromise();
 
-      if (result.error) {
-         throw new Error(`Failed to fetch projects: ${result.error.message}`);
-      }
+    if (result.error) {
+      throw new Error(`Failed to fetch projects: ${result.error.message}`);
+    }
 
-      if (!result.data) {
-         throw new Error("No data received from project query");
-      }
+    if (!result.data) {
+      throw new Error("No data received from project query");
+    }
 
-      return result.data.projects ?? [];
-   };
+    return result.data.projects ?? [];
+  };
 
-   const getShowCaseProjects = async (): Promise<ProjectShowCase[]> => {
-      const result = await client
-         .query<QueryProjectsShowCaseResponse>(queryProjects, {})
-         .toPromise();
+  const getShowCaseProjects = async (): Promise<ProjectShowCase[]> => {
+    const result = await client
+      .query<QueryProjectsShowCaseResponse>(queryProjectsShowcase, {})
+      .toPromise();
 
-      if (result.error) {
-         throw new Error(`Failed to fetch projects: ${result.error.message}`);
-      }
+    if (result.error) {
+      throw new Error(
+        `Failed to fetch showcase projects: ${result.error.message}`,
+      );
+    }
 
-      if (!result.data) {
-         throw new Error("No data received from project query");
-      }
+    if (!result.data) {
+      throw new Error("No data received from showcase projects query");
+    }
 
-      return result.data.projects ?? [];
-   };
+    return result.data.projects ?? [];
+  };
 
-   const getProjectBySlug = (slug: string | undefined, projects: Project[]): Project | null => {
-      const data = projects.find(project => project.slug === slug);
-      return data ?? null;
-   };
+  const getProjectBySlug = (
+    slug: string | undefined,
+    projects: Project[],
+  ): Project | null => {
+    const data = projects.find((project) => project.slug === slug);
+    return data ?? null;
+  };
 
-   return {
-      getAllProjects,
-      getShowCaseProjects,
-      getProjectBySlug,
-   };
+  return {
+    getAllProjects,
+    getShowCaseProjects,
+    getProjectBySlug,
+  };
 }
